106c58ac7a10e4127a3806025f480f09b58f3350
WeBstwr <ifedhawebster@gmail.com>
2024-06-22 13:07:20 +0300

    implement the get products route
    I have implemented the get all users route
    
    I have also implemented the get one user route.

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,9 +1,31 @@
 import { Router } from "express";
 import pool from "./db.config.js";
+
 const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("getting all users");
+router.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM products");
+    res.status(200).json({ success: true, data: result.rows });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const result = await pool.query("SELECT * FROM products WHERE id = $1", [
+      id,
+    ]);
+    if (result.rowCount === 0) {
+      res.status(404).json({ success: false, message: "Product Not Found" });
+    } else {
+      res.status(200).json({ success: true, data: result.rows[0] });
+    }
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
 });
 
 export default router;
